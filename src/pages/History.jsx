4c38eaafa99c93069e44a6bd787b9adf3adaf524
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-04-21 09:54:46 +0630

    history updated by amk

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -74,7 +74,7 @@ const History = () => {
                   {/* <th>ဂိမ်းအခြေအနေ</th> */}
                   <td>{log.closing_balance}</td>
                   <td>{log.type}</td>
-                  <td className='btn btn-outline-warning'>
+                  <td className='badge text-bg-primary text-wrap'>
                     {log.product_name}
                   </td>
                   <td>{log.amount}</td>
